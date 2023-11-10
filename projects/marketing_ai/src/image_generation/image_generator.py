6212432e980e28b3671443eb94bb035738124df0
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-11-10 01:34:57 +0100

    Update views for image generation

--- projects/marketing_ai/src/image_generation/image_generator.py
+++ projects/marketing_ai/src/image_generation/image_generator.py
@@ -19,6 +19,7 @@ from src.config import Config
 # Setup logger
 logger = logging.getLogger(__name__)
 
+
 class ImageGenerator(ABC):
     '''
     Abstract class for generating images from a given prompt
@@ -26,13 +27,13 @@ class ImageGenerator(ABC):
     @classmethod
     def __instancecheck__(cls, instance: Any) -> bool:
         return cls.__subclasscheck__(type(instance))
-    
+
     @classmethod
     def __subclasscheck__(cls, subclass: Any) -> bool:
         return (
             hasattr(subclass, 'generate_image') and
             callable(subclass.generate_image)
-        )   
+        )
 
     @abstractmethod
     def generate_image(self, image_prompt: str, width: int, height: int) -> str:
@@ -113,7 +114,7 @@ class StableDiffusionImageGenerator(ImageGenerator):
         })
 
         headers = {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
@@ -122,12 +123,14 @@ class StableDiffusionImageGenerator(ImageGenerator):
 
         return response.json()["output"][0]
 
+
 class DallEImageGenerator(ImageGenerator):
     """
      A class to generate images from a given prompt using the OpenAI API
     """
     MAX_PROMPT_LENGTH = 1000
     SUPPORTED_SIZES = [256, 512, 1024]
+
     def __init__(self, model_version: str = 'dall-e-3') -> None:
         '''
         Args:
@@ -141,23 +144,24 @@ class DallEImageGenerator(ImageGenerator):
     def _validate_prompt(self, image_prompt: str) -> None:
         if not isinstance(image_prompt, str):
             raise TypeError('Prompt must be a string')
-        
+
         if len(image_prompt) == 0:
             raise ValueError('Prompt must not be empty')
-        
+
         if (len(image_prompt) > self.MAX_PROMPT_LENGTH):
-            raise ValueError(f'Prompt must be less than {self.MAX_PROMPT_LENGTH} characters')
+            raise ValueError(
+                f'Prompt must be less than {self.MAX_PROMPT_LENGTH} characters')
 
     def _validate_size(self, width: int, height: int) -> None:
         if not isinstance(width, int):
             raise TypeError('Width must be an integer')
-        
+
         if not isinstance(height, int):
             raise TypeError('Height must be an integer')
-        
+
         if (width != height):
             raise ValueError('Width and height must be equal')
-        
+
         if (width not in self.SUPPORTED_SIZES):
             raise ValueError(f'Width must be {self.SUPPORTED_SIZES}')
 
@@ -183,19 +187,21 @@ class DallEImageGenerator(ImageGenerator):
         )
         parse_url = response["data"][0]["url"]
 
-        logging.info(f'Generated image from prompt: {image_prompt}, size: {size}, url: {parse_url}')
+        logging.info(
+            f'Generated image from prompt: {image_prompt}, size: {size}, url: {parse_url}')
 
         return parse_url
-    
+
 
 SAVE_PATH = "data/raw_images/"
+
 
 def download_and_save_image(image_url: str, image_name: str) -> None:
     """ Download and save an image from a given URL. """
     try:
         response = requests.get(image_url)
         response.raise_for_status()  # Check if the request was successful
-        
+
         image = Image.open(BytesIO(response.content))
         image.save(SAVE_PATH + image_name + ".png")
         print(f"Image saved to {SAVE_PATH + image_name + '.png'}")
