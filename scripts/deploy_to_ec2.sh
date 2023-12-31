2e97597c98d043801744d76157324b3622630103
Sverre Nystad <sverrenystad@gmail.com>
2023-12-31 02:18:20 +0100

    Build(#4): Create deployment pipeline and bash script to run in production

---
+++ scripts/deploy_to_ec2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# deploy_to_ec2.sh
+
+# Navigate to your Django project directory
+cd /path/to/your/django/project
+
+# Pull the latest code
+git pull origin main
+
+# Install any new dependencies
+pip install -r requirements.txt
+
+# Make database migrations
+python manage.py makemigrations
+
+# Apply database migrations
+python manage.py migrate
+
+# Restart your Django application within a tmux session
+TMUX_SESSION_NAME="django_app"
+
+# Check if the tmux session exists
+if tmux has-session -t $TMUX_SESSION_NAME 2>/dev/null; then
+  # If it exists, kill the session
+  tmux kill-session -t $TMUX_SESSION_NAME
+fi
+
+# Create a new tmux session and run your Django application
+tmux new-session -d -s $TMUX_SESSION_NAME "python manage.py runserver 0.0.0.0:8000"
+
+echo "Deployment completed."
