4ea61eaf326feef9a8d663954a76a179bd17750d
Sverre Nystad <sverrenystad@gmail.com>
2024-01-06 23:12:33 +0100

    Feat: Add MemberApplication model to admin site

--- team/admin.py
+++ team/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, MemberApplication
 
 # Register your models here.
 admin.site.register(Member)
+admin.site.register(MemberApplication)
