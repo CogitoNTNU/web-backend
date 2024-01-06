dff2f2ab2eed184922698a49c286e24417781a74
Sverre Nystad <sverrenystad@gmail.com>
2024-01-06 23:21:02 +0100

    Feat(#28): Add endpoint for applying to cogito

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from team.views import get_members
+from team.views import get_members, apply
 
-urlpatterns = [path("members_by_type/", get_members, name="Members_getter")]
+urlpatterns = [
+    path("members_by_type/", get_members, name="Members_getter"),
+    path("apply/", apply, name="Apply"),
+]
--- team/views.py
+++ team/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import AllowAny
 from django.http import JsonResponse
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from .models import Member
-from .serializers import MemberSerializer, FindMemberSerializer
+from .serializers import (
+    MemberSerializer,
+    FindMemberSerializer,
+    MemberApplicationSerializer,
+)
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -32,13 +37,13 @@ member_error_response = openapi.Response(
     response_description="",
     responses={200: member_success_response, 400: member_error_response},
 )
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([permissions.AllowAny])
 def get_members(request) -> JsonResponse:
-    '''Returns the members wished upon the request'''
+    """Returns the members wished upon the request"""
     try:
-        member_type: str = request.data.get('member_type')
-        if (member_type == "Alle Medlemmer"):
+        member_type: str = request.data.get("member_type")
+        if member_type == "Alle Medlemmer":
             members = Member.objects.all()
         else:
             members = Member.objects.filter(category=member_type)
@@ -53,27 +58,33 @@ def get_members(request) -> JsonResponse:
 
 # Apply
 application_success_response = openapi.Response(
-    description="Sebds",
+    description="Sends an application for membership to Cogito",
     examples={"application/json": {"member_type": "Web Developer"}},
 )
 
 application_error_response = openapi.Response(
-    description="Get the members associated with the 'member_type'",
+    description="The application was not sent in due to missing fields or invalid data",
     examples={"application/json": {"error": "Missing fields"}},
 )
 
 
 @swagger_auto_schema(
     method="POST",
-    request_body=FindMemberSerializer,
+    request_body=MemberApplicationSerializer,
     operation_description="Sends in an application to Cogito",
     tags=["Member Management"],
     response_description="Returns a message confirming that the application has been registered.",
-    responses={200: application_success_response,
-               400: application_error_response},
+    responses={200: application_success_response, 400: application_error_response},
 )
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def apply(request):
-    """Apply for membership status to the organization """
-    print(request.data, flush=True)
-    return None
+    """Apply for membership status to the organization"""
+
+    serializer = MemberApplicationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {"message": "Application sent in successfully"}
+        return Response(message, status=status.HTTP_200_OK)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
