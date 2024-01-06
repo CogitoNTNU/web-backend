668686e4501d5fc7479f183861926ea5f1ebca5a
Sverre Nystad <sverrenystad@gmail.com>
2024-01-06 23:14:57 +0100

    Refactor: Update Member model field formatting

--- team/models.py
+++ team/models.py
@@ -4,15 +4,12 @@ from django.db import models
 
 
 class Member(models.Model):
-    order = models.IntegerField(
-        'Order', primary_key=True, blank=True, default=0)
-    name = models.CharField('Name', max_length=30, blank=True, default='')
-    title = models.CharField('Title', max_length=30, blank=True, default='')
-    image = models.ImageField(
-        'Image', null=True, blank=True, upload_to="images/")
-    category = models.CharField(
-        'Category', max_length=30, blank=True, default='')
-    email = models.EmailField('Email', max_length=50, blank=True, default='')
+    order = models.IntegerField("Order", primary_key=True, blank=True, default=0)
+    name = models.CharField("Name", max_length=30, blank=True, default="")
+    title = models.CharField("Title", max_length=30, blank=True, default="")
+    image = models.ImageField("Image", null=True, blank=True, upload_to="images/")
+    category = models.CharField("Category", max_length=30, blank=True, default="")
+    email = models.EmailField("Email", max_length=50, blank=True, default="")
 
     github = models.URLField(
         'GitHub', max_length=200, blank=True, default='')
