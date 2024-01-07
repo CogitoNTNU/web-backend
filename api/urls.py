6dbad19026dbb8b9eb48ec518b373c6b4d1cd818
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-07 21:58:21 +0100

    Feat: Add project description views to endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from team.views import get_members, apply, get_applications
+from team.views import get_members, apply, get_applications, get_projects, add_project
 
 urlpatterns = [
     path("members_by_type/", get_members, name="Members_getter"),
     path("apply/", apply, name="Apply"),
     path("applications/", get_applications, name="Applications"),
+    path("projects/", get_projects, name="Projects"),
+    path("add_project/", add_project, name="Add Project"),
 ]
