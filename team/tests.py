0f7d8bdd0e0a87b272f5b8bcd1261d91abee1d6f
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-01-12 00:27:24 +0100

    Test: Create tests for apply endpoint Check for valid applications and saving, Check for invalid applications and non saving of these

--- team/tests.py
+++ team/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from team.models import Member
+from team.models import Member, MemberApplication
 
 # Create your tests here.
 
@@ -45,102 +45,156 @@ class GetMembersTestCase(TestCase):
         self.assertEqual(response.content.decode(), "[]")
 
 
-class MemberApplicationTestCase(TestCase):
+class ApplyTestCase(TestCase):
     def setUp(self):
-        # Setup run before every test method.
+        self.client = Client()
         self.url = f"{base}apply/"
 
         self.valid_payload = {
             "first_name": "John",
             "last_name": "Doe",
-            "email": "john.doe@example.com",
+            "email": "johndoe@example.com",
             "phone_number": "1234567890",
+            "about": "I am a developer, who loves AI",
         }
+        self.amount_of_applications_before_test = MemberApplication.objects.count()
 
-    def tearDown(self) -> None:
-        # Clean up run after every test method.
-        pass
+    def test_apply_invalid_email(self):
+        invalid_payload = self.valid_payload.copy()
+        invalid_payload["email"] = "invalid-email"
+
+        response = self.client.post(self.url, invalid_payload, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("email", response.data)
+
+        # Check that the application was not created in the database
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test,
+        )
 
     def test_invalid_missing_email_payload(self):
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "",
-            "phone_number": "1234567890",
-        }
-        response = self.client.post(self.url, data)
+        invalid_payload = self.valid_payload.copy()
+        invalid_payload["email"] = ""
 
+        response = self.client.post(self.url, invalid_payload, format="json")
         self.assertEqual(response.status_code, 400)
+        self.assertIn("email", response.data)
+
+        # Check that the application was not created in the database
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test,
+        )
 
     def test_invalid_missing_first_name_payload(self):
-        data = {
-            "first_name": "",
-            "last_name": "Doe",
-            "email": "user@domain.com",
-            "phone_number": "1234567890",
-        }
-        response = self.client.post(self.url, data)
+        invalid_payload = self.valid_payload.copy()
+        invalid_payload["first_name"] = ""
+
+        response = self.client.post(self.url, invalid_payload, format="json")
         self.assertEqual(response.status_code, 400)
+        # Check that the error message contains the field name
+        self.assertIn("first_name", response.data)
+
+        # Check that the application was not created in the database
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test,
+        )
 
     def test_invalid_missing_last_name_payload(self):
-        data = {
-            "first_name": "John",
-            "last_name": "",
-            "email": "user@domain.com",
-            "phone_number": "1234567890",
-        }
-        response = self.client.post(self.url, data)
+        invalid_payload = self.valid_payload.copy()
+        invalid_payload["last_name"] = ""
+
+        response = self.client.post(self.url, invalid_payload, format="json")
         self.assertEqual(response.status_code, 400)
+        # Check that the error message contains the field name
+        self.assertIn("last_name", response.data)
+
+        # Check that the application was not created in the database
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test,
+        )
 
     def test_invalid_missing_phone_number_payload(self):
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "user@domain.com",
-            "phone_number": "",
-        }
-        response = self.client.post(self.url, data)
-        self.assertEqual(response.status_code, 400)
+        invalid_payload = self.valid_payload.copy()
+        invalid_payload["phone_number"] = ""
 
-    def test_invalid_to_short_phone_number_payload(self):
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "user@domain.com",
-            "phone_number": "123",
-        }
-        response = self.client.post(self.url, data)
+        response = self.client.post(self.url, invalid_payload, format="json")
+        self.assertEqual(response.status_code, 400)
+        # Check that the error message contains the field name
+        self.assertIn("phone_number", response.data)
+
+        # Check that the application was not created in the database
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test,
+        )
+
+    def test_apply_successful(self):
+        response = self.client.post(self.url, self.valid_payload, format="json")
         self.assertEqual(response.status_code, 200)
+        self.assertIn("Application sent in successfully", response.data["message"])
+
+        # Check that the application was created in the database
+        self.assertTrue(
+            MemberApplication.objects.filter(email="johndoe@example.com").exists()
+        )
+        # Check that the amount of applications in the database has increased by 1
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test + 1,
+        )
+
+    def test_short_phone_number_payload(self):
+        payload = self.valid_payload.copy()
+        payload["phone_number"] = "123"
+
+        response = self.client.post(self.url, payload, format="json")
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the amount of applications in the database has increased by 1
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test + 1,
+        )
 
     def test_international_phone_numbers(self):
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "user@domain.com",
-            "phone_number": "+491234567890",
-        }
-        response = self.client.post(self.url, data)
+        payload = self.valid_payload.copy()
+        payload["phone_number"] = "+491234567890"
+
+        response = self.client.post(self.url, payload, format="json")
         self.assertEqual(response.status_code, 200)
+
+        # Check that the amount of applications in the database has increased by 1
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test + 1,
+        )
 
     def test_valid_long_about_section_payload(self):
-        about_section: str = "Lorem ipsum" * 1000
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "user@domain.com",
-            "phone_number": "1234567890",
-            "about": about_section,
-        }
-        response = self.client.post(self.url, data)
+        payload = self.valid_payload.copy()
+        payload["about"] = "Lorem ipsum" * 1000
+
+        response = self.client.post(self.url, payload, format="json")
         self.assertEqual(response.status_code, 200)
 
+        # Check that the amount of applications in the database has increased by 1
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test + 1,
+        )
+
     def test_valid_empty_about_section_payload(self):
-        data = {
-            "first_name": "John",
-            "last_name": "Doe",
-            "email": "user@domain.com",
-            "phone_number": "1234567890",
-            "about": "",
-        }
-        response = self.client.post(self.url, data)
+        payload = self.valid_payload.copy()
+        payload["about"] = ""
+
+        response = self.client.post(self.url, payload, format="json")
         self.assertEqual(response.status_code, 200)
+
+        # Check that the amount of applications in the database has increased by 1
+        self.assertEqual(
+            MemberApplication.objects.count(),
+            self.amount_of_applications_before_test + 1,
+        )
