d3e265167dfcd4295cf004c26365875cacd75277
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-01-13 01:19:14 +0100

    Feat: Add new URLs for project descriptions and add project description view

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from team.views import get_members, apply, get_applications, get_projects_descriptions, add_project_description
+from team.views import (
+    get_members,
+    apply,
+    get_applications,
+    get_projects_descriptions,
+    add_project_description,
+)
 from .views import health_check
 
 urlpatterns = [
@@ -7,4 +13,12 @@ urlpatterns = [
     path("apply/", apply, name="Apply"),
     path("applications/", get_applications, name="Applications"),
     path("health-check/", health_check, name="Health_check"),
+    path(
+        "projects-description/", get_projects_descriptions, name="Project_description"
+    ),
+    path(
+        "add-project-description/",
+        add_project_description,
+        name="Add_project_description",
+    ),
 ]
