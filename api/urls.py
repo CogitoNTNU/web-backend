d3b672136a308c7f968d6b293c281b228d96932b
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-14 23:16:39 +0100

    Feat: Add get_images view to api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from projects.views import generate_image_view
+from projects.views import generate_image_view, get_images
 from team.views import get_members, apply, get_applications, get_projects_descriptions, add_project_description
 from .views import health_check
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("applications/", get_applications, name="Applications"),
     path("health-check/", health_check, name="Health_check"),
     path("projects/marketing-ai/", generate_image_view, name="Generate_image"),
+    path("projects/marketing-ai/images", get_images, name="Get_images"),
 ]
