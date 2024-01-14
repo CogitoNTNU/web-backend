89ceb01e26e905231497a1524dd58549bdc7f91e
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-14 23:16:17 +0100

    Fix: Update Image model field name to image_url

--- projects/models.py
+++ projects/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class Image(models.Model):
-    image = models.CharField(max_length=1000, blank=True, default="", help_text=" The image url")
+    image_url = models.CharField(max_length=1000, blank=True, default="", help_text=" The image url")
     prompt = models.CharField(max_length=1000, blank=True, default="", help_text=" The prompt used to generate the image")
     date_of_generation = models.DateTimeField(auto_now_add=True, help_text="The date and time the image was generated")
     height = models.IntegerField(blank=True, default=0, help_text="The height of the image")
     width = models.IntegerField(blank=True, default=0, help_text="The width of the image")
 
     def __str__(self) -> str:
-        return self.image.url
+        return self.image_url
     
