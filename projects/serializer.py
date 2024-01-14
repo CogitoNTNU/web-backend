8cec8199f7b37da0c8237fcd113c09e52eccd4ee
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-14 23:02:50 +0100

    Docs: Update width and height help text in CreateImageSerializer

--- projects/serializer.py
+++ projects/serializer.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 
 class CreateImageSerializer(serializers.Serializer):
     prompt = serializers.CharField(help_text="The prompt to generate an image from")
-    width = serializers.IntegerField(help_text="The width of the image")
-    height = serializers.IntegerField(help_text="The height of the image")
+    width = serializers.IntegerField(help_text="The width of the image. Width and height must be 1024x1024, 1792x1024, or 1024x1792")
+    height = serializers.IntegerField(help_text="The height of the image. Width and height must be 1024x1024, 1792x1024, or 1024x1792")
