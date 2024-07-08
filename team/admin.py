acc0f6ab3bcf46ca2a58a35c7d2f312640071f65
Sverre Nystad <sverrenystad@gmail.com>
2024-07-08 21:58:39 +0200

    feat: make date of applicants a read only field in admin panel

--- team/admin.py
+++ team/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-from .models import Member, MemberApplication, ProjectDescription, MemberCategory
+from .models import (
+    Member,
+    MemberApplication,
+    ProjectDescription,
+    MemberCategory,
+)
+
+
+class MemberApplicationAdmin(admin.ModelAdmin):
+    # Fields to be displayed in the admin panel even they are read-only
+    fields = [
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+        "about",
+        "date_of_application",
+    ]
+    # Fields that are read-only in the admin panel
+    readonly_fields = [
+        "date_of_application",
+    ]
+
 
 # Register your models here.
 admin.site.register(Member)
-admin.site.register(MemberApplication)
+admin.site.register(MemberApplication, MemberApplicationAdmin)
 admin.site.register(MemberCategory)
 admin.site.register(ProjectDescription)
