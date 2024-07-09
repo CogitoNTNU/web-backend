67ffb84f148168712323faceb0c8c80fdde18e3f
Sverre Nystad <sverrenystad@gmail.com>
2024-07-09 23:31:47 +0200

    feat: add debug and django secret key to env variables

--- cogito/settings.py
+++ cogito/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rwadhz7=p5)$!m*h)tl8q$v7atu_rf=7-=gh1@!y@x30@de#!-"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = [
     "16.171.144.138",
