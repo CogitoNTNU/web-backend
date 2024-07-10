d32ad4c1a4e72349768e47a0bfa523edf518da1a
Sverre Nystad <sverrenystad@gmail.com>
2024-07-10 19:03:20 +0200

    build: add correct email host and port

--- cogito/settings.py
+++ cogito/settings.py
@@ -109,8 +109,8 @@ DATABASES = {
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = "smtp.proisp.no"
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
