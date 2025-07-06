1a796915c49eddc11b27fa73aa2655744d091a09
Sverre Nystad <sverrenystad@gmail.com>
2025-07-06 18:53:13 +0200

    feat: add restore database script for restoring backups

---
+++ scripts/restore-databse.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Variables
+DUMP_FILE_NAME="$1"
+# Check if dump file is provided
+if [ -z "$DUMP_FILE_NAME" ]; then
+  echo "Usage: ./restore.sh path_to_dump_file.sql"
+  exit 1
+fi
+
+BACKUP_DIR="./backups"
+DUMP_FILE="$BACKUP_DIR/$DUMP_FILE_NAME"
+
+# Check if dump file exists
+if [ ! -f "$DUMP_FILE" ]; then
+  echo "File not found: $DUMP_FILE"
+  exit 1
+fi
+
+# Restore the dump
+docker exec -i cogito_db psql -U cogitouser -d cogitodb < $DUMP_FILE
+
+echo "Restore completed from: $DUMP_FILE to cogito_db"
+
+# Display the database tables
+echo "Database tables:"
+docker exec -it cogito_db psql -U cogitouser -d cogitodb -c "\dt"
