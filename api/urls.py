3807ad9373fc9fd9e387e3f5d22f412b08c286ef
Sverre Nystad <sverrenystad@gmail.com>
2025-07-20 11:46:06 +0200

    refactor: remove redundant view and use django rest framework

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from team.views import (
     ProjectsView,
     get_members,
     apply,
-    get_applications,
+    ApplicationsView,
     UpdateMemberImageView,
     MemberCategoryView,
 )
@@ -12,7 +12,7 @@ from api.views import health_check
 urlpatterns = [
     path("members-by-type/", get_members, name="Members_getter"),
     path("apply/", apply, name="Apply"),
-    path("applications/", get_applications, name="Applications"),
+    path("applications/", ApplicationsView.as_view(), name="Applications"),
     path("health-check/", health_check, name="Health_check"),
     path("member/image", UpdateMemberImageView.as_view(), name="Update_member_image"),
     path("member/category", MemberCategoryView.as_view(), name="Member_category"),
